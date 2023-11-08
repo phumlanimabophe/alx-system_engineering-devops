24b8dd4ab415b8265bad89b0542387384d1b6c89
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-11-08 17:06:24 +0000

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,5 @@
-# Define an Exec resource to fix the WordPress file
-exec { 'fix-wordpress':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/',
-  creates => '/var/www/html/wp-settings.php',
-}
-
-# Ensure that the Apache service is restarted when the file is modified
-service { 'apache2':
-  ensure  => running,
-  require => Exec['fix-wordpress'],
+# Fix setting in Wordpress
+exec { 'settingPress':
+  command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+  provider => shell,
 }
